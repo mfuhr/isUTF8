50487307dfd102b50ee889841c819a7109f7ff64
Michael Fuhr <mike@fuhr.org>
2017-05-29 08:06:51 -0600

    Added tests for short UTF-8 sequences

--- isUTF8_test.go
+++ isUTF8_test.go
@@ -26,3 +26,51 @@ func TestAllUTF8(t *testing.T) {
   }
 }
 
+func TestUTF8Short2ByteC2(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_2byte_c2.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short3ByteE0(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_3byte_e0.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short3ByteE1(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_3byte_e1.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short3ByteED(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_3byte_ed.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short3ByteEE(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_3byte_ee.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short4ByteF0(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_4byte_f0.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short4ByteF1(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_4byte_f1.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
+func TestUTF8Short4ByteF4(t *testing.T) {
+  if isUTF8 := fileIsUTF8("test_utf8_short_4byte_f4.txt"); isUTF8 {
+    t.Fail()
+  }
+}
+
