283cd3c13d1bd8847399ea63d10cfb061aad1a66
Michael Fuhr <mike@fuhr.org>
2017-05-29 18:14:44 -0600

    Reformatted to standard Go formatting

--- isUTF8.go
+++ isUTF8.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "golang.org/x/sys/unix"
+	"fmt"
+	"golang.org/x/sys/unix"
+	"log"
+	"os"
 )
 
 const MaxInt = int(^uint(0) >> 1)
@@ -18,191 +18,191 @@ const MaxInt = int(^uint(0) >> 1)
 // http://www.unicode.org/versions/Unicode9.0.0/ch03.pdf#G7404
 //
 func bufferIsUTF8(fd int, offset int64, length int, checkSize int) (bool, int64) {
-  if checkSize > length {
-    log.Fatalf("checkSize (%v) > length (%v)", checkSize, length)
-  }
+	if checkSize > length {
+		log.Fatalf("checkSize (%v) > length (%v)", checkSize, length)
+	}
 
-  idxStart := offset % int64(unix.Getpagesize())
-  idxEnd := checkSize + int(idxStart)
-  mapOffset := offset - idxStart
-  mapLength := length + int(idxStart)
-  //fmt.Printf("offset=%v, mapOffset=%v, idxStart=%v, idxEnd=%v, length=%v, mapLength=%v\n",
-  //           offset, mapOffset, idxStart, idxEnd, length, mapLength)
+	idxStart := offset % int64(unix.Getpagesize())
+	idxEnd := checkSize + int(idxStart)
+	mapOffset := offset - idxStart
+	mapLength := length + int(idxStart)
+	//fmt.Printf("offset=%v, mapOffset=%v, idxStart=%v, idxEnd=%v, length=%v, mapLength=%v\n",
+	//           offset, mapOffset, idxStart, idxEnd, length, mapLength)
 
-  buf, err := unix.Mmap(fd, mapOffset, mapLength, unix.PROT_READ, unix.MAP_PRIVATE)
-  if err != nil {
-    log.Fatalf("Mmap(%v, %v): %s", offset, length, err)
-  }
+	buf, err := unix.Mmap(fd, mapOffset, mapLength, unix.PROT_READ, unix.MAP_PRIVATE)
+	if err != nil {
+		log.Fatalf("Mmap(%v, %v): %s", offset, length, err)
+	}
 
-  bufSize := len(buf)
-  if bufSize != mapLength {
-    log.Fatalf("bufSize (%v) != mapLength (%v)", bufSize, mapLength)
-  }
+	bufSize := len(buf)
+	if bufSize != mapLength {
+		log.Fatalf("bufSize (%v) != mapLength (%v)", bufSize, mapLength)
+	}
 
-  isUTF8 := true
+	isUTF8 := true
 
-  var i int = int(idxStart)
-  for i < idxEnd {
-    if buf[i] <= 0x7f { // U+0000..U+007F
-      i++
-    } else if buf[i] >= 0xc2 && buf[i] <= 0xdf {  // U+0080..U+07FF
-      if i + 1 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0xbf {
-        i += 2
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] == 0xe0 {  // U+0800..U+0FFF
-      if i + 2 < bufSize &&
-         buf[i + 1] >= 0xa0 && buf[i + 1] <= 0xbf &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf {
-        i += 3
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] >= 0xe1 && buf[i] <= 0xec {  // U+1000..U+CFFF
-      if i + 2 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0xbf &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf {
-        i += 3
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] == 0xed {  // U+D000..U+D7FF
-      if i + 2 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0x9f &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf {
-        i += 3
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] >= 0xee && buf[i] <= 0xef {  // U+E000..U+FFFF
-      if i + 2 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0xbf &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf {
-        i += 3
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] == 0xf0 {  // U+10000..U+3FFFF
-      if i + 3 < bufSize &&
-         buf[i + 1] >= 0x90 && buf[i + 1] <= 0xbf &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf &&
-         buf[i + 3] >= 0x80 && buf[i + 3] <= 0xbf {
-        i += 4
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] >= 0xf1 && buf[i] <= 0xf3 {  // U+40000..U+FFFFF
-      if i + 3 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0xbf &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf &&
-         buf[i + 3] >= 0x80 && buf[i + 3] <= 0xbf {
-        i += 4
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else if buf[i] == 0xf4 {  // U+100000..U+10FFFF
-      if i + 3 < bufSize &&
-         buf[i + 1] >= 0x80 && buf[i + 1] <= 0x8f &&
-         buf[i + 2] >= 0x80 && buf[i + 2] <= 0xbf &&
-         buf[i + 3] >= 0x80 && buf[i + 3] <= 0xbf {
-        i += 4
-      } else {
-        isUTF8 = false
-        break
-      }
-    } else {
-      isUTF8 = false
-      break
-    }
-  }
+	var i int = int(idxStart)
+	for i < idxEnd {
+		if buf[i] <= 0x7f { // U+0000..U+007F
+			i++
+		} else if buf[i] >= 0xc2 && buf[i] <= 0xdf { // U+0080..U+07FF
+			if i+1 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0xbf {
+				i += 2
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] == 0xe0 { // U+0800..U+0FFF
+			if i+2 < bufSize &&
+				buf[i+1] >= 0xa0 && buf[i+1] <= 0xbf &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf {
+				i += 3
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] >= 0xe1 && buf[i] <= 0xec { // U+1000..U+CFFF
+			if i+2 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0xbf &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf {
+				i += 3
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] == 0xed { // U+D000..U+D7FF
+			if i+2 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0x9f &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf {
+				i += 3
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] >= 0xee && buf[i] <= 0xef { // U+E000..U+FFFF
+			if i+2 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0xbf &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf {
+				i += 3
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] == 0xf0 { // U+10000..U+3FFFF
+			if i+3 < bufSize &&
+				buf[i+1] >= 0x90 && buf[i+1] <= 0xbf &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf &&
+				buf[i+3] >= 0x80 && buf[i+3] <= 0xbf {
+				i += 4
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] >= 0xf1 && buf[i] <= 0xf3 { // U+40000..U+FFFFF
+			if i+3 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0xbf &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf &&
+				buf[i+3] >= 0x80 && buf[i+3] <= 0xbf {
+				i += 4
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else if buf[i] == 0xf4 { // U+100000..U+10FFFF
+			if i+3 < bufSize &&
+				buf[i+1] >= 0x80 && buf[i+1] <= 0x8f &&
+				buf[i+2] >= 0x80 && buf[i+2] <= 0xbf &&
+				buf[i+3] >= 0x80 && buf[i+3] <= 0xbf {
+				i += 4
+			} else {
+				isUTF8 = false
+				break
+			}
+		} else {
+			isUTF8 = false
+			break
+		}
+	}
 
-  err = unix.Munmap(buf)
-  if err != nil {
-    log.Fatalf("Munmap: %s", err)
-  }
+	err = unix.Munmap(buf)
+	if err != nil {
+		log.Fatalf("Munmap: %s", err)
+	}
 
-  return isUTF8, int64(i) - idxStart
+	return isUTF8, int64(i) - idxStart
 }
 
 func fileIsUTF8(fname string) bool {
-  f, err := unix.Open(fname, unix.O_RDONLY, 0)
-  if err != nil {
-    log.Fatalf("Open: %s: %s", fname, err)
-  }
+	f, err := unix.Open(fname, unix.O_RDONLY, 0)
+	if err != nil {
+		log.Fatalf("Open: %s: %s", fname, err)
+	}
 
-  defer unix.Close(f)
+	defer unix.Close(f)
 
-  var sbuf unix.Stat_t
-  err = unix.Fstat(f, &sbuf)
-  if err != nil {
-    log.Fatalf("Fstat: %s: %s", fname, err)
-  } else if (sbuf.Mode & unix.S_IFREG) == 0 {
-    log.Fatalf("%s: not a regular file", fname)
-  }
+	var sbuf unix.Stat_t
+	err = unix.Fstat(f, &sbuf)
+	if err != nil {
+		log.Fatalf("Fstat: %s: %s", fname, err)
+	} else if (sbuf.Mode & unix.S_IFREG) == 0 {
+		log.Fatalf("%s: not a regular file", fname)
+	}
 
-  fileSize := sbuf.Size
-  var mapOffset int64 = 0
-  maxMapSize := MaxInt - unix.Getpagesize() // leave room for alignment
-  maxCharSize := 4
+	fileSize := sbuf.Size
+	var mapOffset int64 = 0
+	maxMapSize := MaxInt - unix.Getpagesize() // leave room for alignment
+	maxCharSize := 4
 
-  if maxMapSize < maxCharSize {
-    log.Fatalf("maxMapSize (%v) < maxCharSize (%v)", maxMapSize, maxCharSize)
-  }
+	if maxMapSize < maxCharSize {
+		log.Fatalf("maxMapSize (%v) < maxCharSize (%v)", maxMapSize, maxCharSize)
+	}
 
-  for mapOffset < fileSize {
-    bytesLeft := fileSize - mapOffset
+	for mapOffset < fileSize {
+		bytesLeft := fileSize - mapOffset
 
-    var mapSize int
-    var checkSize int
+		var mapSize int
+		var checkSize int
 
-    if bytesLeft > int64(maxMapSize) {
-      mapSize = maxMapSize
-      checkSize = mapSize - maxCharSize
-    } else {
-      mapSize = int(bytesLeft)
-      checkSize = mapSize
-    }
+		if bytesLeft > int64(maxMapSize) {
+			mapSize = maxMapSize
+			checkSize = mapSize - maxCharSize
+		} else {
+			mapSize = int(bytesLeft)
+			checkSize = mapSize
+		}
 
-    //fmt.Printf("fileSize=%v, mapOffset=%v, bytesLeft=%v, mapSize=%v, checkSize=%v\n",
-    //            fileSize, mapOffset, bytesLeft, mapSize, checkSize)
+		//fmt.Printf("fileSize=%v, mapOffset=%v, bytesLeft=%v, mapSize=%v, checkSize=%v\n",
+		//            fileSize, mapOffset, bytesLeft, mapSize, checkSize)
 
-    isUTF8, bytesChecked := bufferIsUTF8(f, mapOffset, mapSize, checkSize)
-    //fmt.Printf("isUTF8=%v, bytesChecked=%v\n", isUTF8, bytesChecked)
+		isUTF8, bytesChecked := bufferIsUTF8(f, mapOffset, mapSize, checkSize)
+		//fmt.Printf("isUTF8=%v, bytesChecked=%v\n", isUTF8, bytesChecked)
 
-    if !isUTF8 {
-      return false
-    }
+		if !isUTF8 {
+			return false
+		}
 
-    mapOffset += bytesChecked
-    //fmt.Printf("mapOffset=%v\n\n", mapOffset)
-  }
+		mapOffset += bytesChecked
+		//fmt.Printf("mapOffset=%v\n\n", mapOffset)
+	}
 
-  return true
+	return true
 }
 
 func main() {
-  exitCode := 0
+	exitCode := 0
 
-  if len(os.Args) > 2 {
-    log.Printf("warning: multiple filename arguments\n")
-  }
+	if len(os.Args) > 2 {
+		log.Printf("warning: multiple filename arguments\n")
+	}
 
-  for _, arg := range os.Args[1:] {
-    isUTF8 := fileIsUTF8(arg)
-    fmt.Printf("%v %s\n", isUTF8, arg)
-    if !isUTF8 {
-      exitCode = 1
-    }
-  }
+	for _, arg := range os.Args[1:] {
+		isUTF8 := fileIsUTF8(arg)
+		fmt.Printf("%v %s\n", isUTF8, arg)
+		if !isUTF8 {
+			exitCode = 1
+		}
+	}
 
-  os.Exit(exitCode)
+	os.Exit(exitCode)
 }
--- isUTF8_test.go
+++ isUTF8_test.go
@@ -1,81 +1,80 @@
 package main
 
 import (
-  "path/filepath"
-  "testing"
+	"path/filepath"
+	"testing"
 )
 
 const testDir = "testdata"
 
 func TestUTF8(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8.txt")); !isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8.txt")); !isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestLatin1(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_latin1.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_latin1.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestASCII(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_ascii.txt")); !isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_ascii.txt")); !isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestAllUTF8(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_all_utf8.txt")); !isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_all_utf8.txt")); !isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short2ByteC2(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_2byte_c2.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_2byte_c2.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short3ByteE0(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e0.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e0.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short3ByteE1(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e1.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e1.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short3ByteED(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ed.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ed.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short3ByteEE(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ee.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ee.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short4ByteF0(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f0.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f0.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short4ByteF1(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f1.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f1.txt")); isUTF8 {
+		t.Fail()
+	}
 }
 
 func TestUTF8Short4ByteF4(t *testing.T) {
-  if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f4.txt")); isUTF8 {
-    t.Fail()
-  }
+	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f4.txt")); isUTF8 {
+		t.Fail()
+	}
 }
-
