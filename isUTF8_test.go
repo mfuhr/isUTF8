11f26a567fb7af2f90903092be778ca07bb8360c
Michael Fuhr <mike@fuhr.org>
2017-05-29 22:38:46 -0600

    Moved common testing code to a separate function

--- isUTF8_test.go
+++ isUTF8_test.go
@@ -7,74 +7,56 @@ import (
 
 const testDir = "testdata"
 
-func TestUTF8(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8.txt")); !isUTF8 {
+func testFile(t *testing.T, fileName string, expectedResult bool) {
+	if fileIsUTF8(filepath.Join(testDir, fileName)) != expectedResult {
 		t.Fail()
 	}
+}
+
+func TestUTF8(t *testing.T) {
+	testFile(t, "test_utf8.txt", true)
 }
 
 func TestLatin1(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_latin1.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_latin1.txt", false)
 }
 
 func TestASCII(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_ascii.txt")); !isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_ascii.txt", true)
 }
 
 func TestAllUTF8(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_all_utf8.txt")); !isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_all_utf8.txt", true)
 }
 
 func TestUTF8Short2ByteC2(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_2byte_c2.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_2byte_c2.txt", false)
 }
 
 func TestUTF8Short3ByteE0(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e0.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_3byte_e0.txt", false)
 }
 
 func TestUTF8Short3ByteE1(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_e1.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_3byte_e1.txt", false)
 }
 
 func TestUTF8Short3ByteED(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ed.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_3byte_ed.txt", false)
 }
 
 func TestUTF8Short3ByteEE(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_3byte_ee.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_3byte_ee.txt", false)
 }
 
 func TestUTF8Short4ByteF0(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f0.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_4byte_f0.txt", false)
 }
 
 func TestUTF8Short4ByteF1(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f1.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_4byte_f1.txt", false)
 }
 
 func TestUTF8Short4ByteF4(t *testing.T) {
-	if isUTF8 := fileIsUTF8(filepath.Join(testDir, "test_utf8_short_4byte_f4.txt")); isUTF8 {
-		t.Fail()
-	}
+	testFile(t, "test_utf8_short_4byte_f4.txt", false)
 }
